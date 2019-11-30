d5e6076d89ed3a13cfb516ad3f3dc158076815e2
boydwo <marcos.t@ufv.br>
2019-11-30 17:37:10 -0300

    possivel erro

--- arranjo/TAD_Palavra.c
+++ arranjo/TAD_Palavra.c
@@ -1,32 +1,38 @@
 #include "TAD_Palavra.h"
 
-void inicializaPalavra(TPalavra* palavra){
+void inicializaPalavra(TPalavra *palavra)
+{
     palavra->Primeiro = INICIO;
     palavra->Ultimo = palavra->Primeiro;
     palavra->tamanho = 0;
 }
 
-void insereLetra(TPalavra* palavra, TLetra letra){
-    if(palavra->Ultimo > MAX_TAM_PALAVRA)
+void insereLetra(TPalavra *palavra, TLetra letra)
+{
+    if (palavra->Ultimo > MAX_TAM_PALAVRA)
         return;
-    else{
-        palavra->palavra[palavra->Ultimo-1] = letra;
+    else
+    {
+        palavra->palavra[palavra->Ultimo - 1] = letra; // possivel erro
         palavra->Ultimo++;
         palavra->tamanho++;
     }
 }
 
-void removeLetra(TPalavra* palavra){
+void removeLetra(TPalavra *palavra)
+{
     palavra->Ultimo--;
-    palavra->tamanho--;
+    palavra->tamanho--; //possivel erro
 }
 
-void imprimePalavra(TPalavra palavra){
+void imprimePalavra(TPalavra palavra)
+{
     int i;
-    for(i = palavra.Primeiro; i< palavra.Ultimo; i++ )
+    for (i = palavra.Primeiro; i < palavra.Ultimo; i++)
         printf("%d\n", palavra.palavra[i].letra);
 }
 
-int tamanhoPalavra(TPalavra palavra){
+int tamanhoPalavra(TPalavra palavra)
+{
     return palavra.tamanho;
 }
