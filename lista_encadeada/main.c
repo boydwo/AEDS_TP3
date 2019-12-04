3131ca2309239416f0ef82923a36b5232e5ea583
Lucas Takeshi <LTaka@users.noreply.github.com>
2019-12-03 21:58:03 -0300

    encadeada

--- lista_encadeada/main.c
+++ lista_encadeada/main.c
@@ -1,21 +1,23 @@
 #include "TAD_Biblioteca.h"
 int  main ()
 {   
-    int TamPalavra,i,TamTexto,cont=0,QntTexto,j;
+    int TamPalavra,i,TamTexto,cont=0,QntTexto,j,somaQnt;
     TPalavra pPalavra;
     TItemLetra LetraAll;
     TTexto pTexto;
-    TBiblioteca pBbt;
-    
+    TBiblioteca pBbt,pCopiaBbt;
     char ALL;
     inicializaBbt(&pBbt);
+    inicializaBbt(&pCopiaBbt);
     printf("\nDigite a quantidade de textos: ");
     scanf("%d",&QntTexto);
+    somaQnt=0;
      for(j=0;j<QntTexto;j++){
          cont=0;
         inicializaTexto(&pTexto);
         printf("Digite a quantidade de palavra: ");
         scanf("%d",&TamTexto);
+            somaQnt= somaQnt+TamTexto;
         do{
             inicializaPalavra(&pPalavra);
             TamPalavra = 1 + rand ()% 7 ;
@@ -28,8 +30,11 @@ int  main ()
             cont++;
         }while(cont != TamTexto);
         insereBbt(&pBbt,pTexto);
+        insereBbt(&pCopiaBbt,pTexto);
      }
-     imprimeBbt(&pBbt);
+     printf("Bilioteca desordenado:\n");
+     imprimeBbt(&pBbt);   
+
 
 
 //
@@ -130,4 +135,85 @@ int  main ()
 
     return  0 ;
 }
+// typedef struct lista {
+//     elemento valor;
+//     struct lista *prox;
+// } tlista;
 
+// int impl_cmp(elemento a, elemento b);
+
+// void partition(tlista *lista, elemento pivot, tlista **l_ret, tlista **ge_ret) {
+//     tlista *l_tail = NULL;
+//     tlista *ge_tail = NULL;
+
+//     tlista *atual = lista;
+
+//     while (atual != NULL) {
+//         tlista *prox = atual->prox;
+
+//         atual->prox = NULL;
+//         if (impl_cmp(atual->valor, pivot) < 0) {
+//             if (l_tail == NULL) {
+//                  l_tail = atual;
+//                  *l_ret = l_tail;
+//             } else {
+//                 l_tail->prox = atual;
+//                 l_tail = atual;
+//             }
+//         } else {
+//             if (ge_tail == NULL) {
+//                  ge_tail = atual;
+//                  *ge_ret = ge_tail;
+//             } else {
+//                 ge_tail->prox = atual;
+//                 ge_tail = atual;
+//             }
+//         }
+//         atual = prox;
+//     }
+// }
+
+// tlista *concatena_3_listas(tlista *a, tlista *b, tlista *c) {
+//     tlista *head = a;
+//     tlista *tail = head;
+
+//     if (head != NULL) {
+//         head = tail = b;
+//     } else {
+//         while (tail->prox != NULL)  {
+//             tail = tail->prox;
+//         }
+//         tail->prox = b;
+//     }
+
+//     if (head != NULL) {
+//         head = tail = c;
+//     } else {
+//         while (tail->prox != NULL)  {
+//             tail = tail->prox;
+//         }
+//         tail->prox = c;
+//     }
+
+//     return head;
+// }
+
+// tlista *quicksort(tlista *lista) {
+//     /* vou pegar como pivot sempre o primeiro da lista, removendo-o de lá; poderia usar outra técnica, mas essa é boa o suficiente para o exemplo */
+//    tlista *pivot_lista = lista;
+//    elemento pivot = pivot_lista->valor;
+
+//    lista = lista->prox;
+//    pivot_lista->prox = NULL;
+
+//    tlista *menor, *maior;
+
+//    menor = maior = NULL;
+
+//    partition(lista, pivot, &menor, &maior);
+
+//    menor = quicksort(menor);
+//    maior = quicksort(maior);
+
+//   return concatena_3_listas(menor, pivot_lista, maior);
+// }
